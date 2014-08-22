a8ee09ccc15196304b18d0f5d6f723e1fb611cd5
unknown <Administrator@MOB417.corp.spatialinfo.com>
2014-08-22 16:10:19 +1000

    key presses

--- packages/ad/src/_impl.dart
+++ packages/ad/src/_impl.dart
@@ -1,6 +1,7 @@
 part of ad;
 
 abstract class State {
+  Ad parent;
   void render(CanvasRenderingContext2D ctx);
   void update(num dt);
 }
@@ -10,10 +11,22 @@ class Ad {
   CanvasRenderingContext2D ctx;
   State state;
   
+  Set<int> currentlyPressedKeys = new Set<int>();
+  
   Ad(String canvasID, State state) {
     canvas = querySelector('#' + canvasID);
     ctx = canvas.getContext('2d');
     this.state = state;
+    this.state.parent = this;
+    
+    // Hook into the window's onKeyDown and onKeyUp streams to track key states
+    window.onKeyDown.listen((KeyboardEvent event) {
+      currentlyPressedKeys.add(event.keyCode);
+    });
+
+    window.onKeyUp.listen((event) {
+      currentlyPressedKeys.remove(event.keyCode);
+    });
     
     window.requestAnimationFrame(update);
   }
@@ -21,7 +34,6 @@ class Ad {
   void update(num dt) {
     state.update(dt);
     state.render(ctx);
-    window.requestAnimationFrame(update);
-    
+    window.requestAnimationFrame(update); 
   }
 }

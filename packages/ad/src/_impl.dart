44369e4fd5d29f6b72e2b3e724b9641cb20e3c30
unknown <Administrator@MOB417.corp.spatialinfo.com>
2014-08-22 15:31:24 +1000

    More

--- packages/ad/src/_impl.dart
+++ packages/ad/src/_impl.dart
@@ -1,22 +1,26 @@
 part of ad;
 
+abstract class State {
+  void render(CanvasRenderingContext2D ctx);
+  void update(num dt);
+}
+
 class Ad {
   CanvasElement canvas;
   CanvasRenderingContext2D ctx;
+  State state;
   
-  Ad(String canvasID) {
+  Ad(String canvasID, State state) {
     canvas = querySelector('#' + canvasID);
     ctx = canvas.getContext('2d');
+    this.state = state;
     
     window.requestAnimationFrame(update);
   }
   
   void update(num dt) {
-    print(dt);
-    
-    ctx.fillStyle = '#00ff00';
-    ctx.fillRect(0, 0, 32, 32);
-    
+    state.update(dt);
+    state.render(ctx);
     window.requestAnimationFrame(update);
     
   }

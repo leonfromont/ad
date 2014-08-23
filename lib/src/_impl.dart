706b08becb562eb755ea3940039655d2fbf77202
unknown <leon.fromont@gmail.com>
2014-08-23 10:22:26 +1000

    Add pathing

--- lib/src/_impl.dart
+++ lib/src/_impl.dart
@@ -1,5 +1,75 @@
 part of ad;
 
+class Path {
+  num dt;
+  num currentTime;
+  List<Vector> points;
+  Vector a, b;
+  int a_index = 0;
+
+  Path(List<Vector> points, num dt) {
+    if(points.length < 2) {
+      throw new Exception('must have at least two control points');
+    }
+
+    this.points = points;
+    this.dt = dt;
+    this.currentTime = 0.0;
+    this.a = points[0];
+    this.b = points[1];
+  }
+
+  Vector currentPoint() {
+    num t = currentTime / dt;
+    return a.lerp(b, t);
+  }
+
+  void update(num nextdt) {
+    currentTime += nextdt;
+
+    if(currentTime > dt) {
+      currentTime = 0.0;
+      a_index += 1; 
+      a = points[a_index % points.length];
+      b = points[(a_index + 1) % points.length];
+    }
+  }
+}
+
+class Vector {
+  num x, y;
+  Vector(this.x, this.y);
+  
+  operator +(Vector other) => new Vector(x + other.x, y + other.y);
+  operator -(Vector other) => new Vector(x - other.x, y - other.y);
+
+  Vector lerp(Vector other, num dt) {
+    Vector result = new Vector(0, 0);
+    result.x = this.x + dt * (other.x - this.x);
+    result.y = this.y + dt * (other.y - this.y);
+    return result;
+  }
+}
+
+class Rect {
+  num left, top, width, height;
+
+  Rect(this.left, this.top, this.width, this.height);
+
+  num get right             => left + width;
+      set right(num value)  => left = value - width;
+  num get bottom            => top + height;
+      set bottom(num value) => top = value - height;
+      
+  bool collide(Rect other) {
+    bool noverlap = (this.left > other.right || 
+                    other.left > this.right || 
+                    this.top > other.bottom || 
+                        other.top > this.bottom);
+    return !noverlap;
+  }
+}
+
 abstract class State {
   Ad parent;
   void render(CanvasRenderingContext2D ctx);
